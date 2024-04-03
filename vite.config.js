222a2934bd768960d54a0f58894762f4c44eb3db
gaurav parsar <98256253+INDIANgaurav@users.noreply.github.com>
2024-04-03 16:01:20 +0530

    v3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy other requests to your index.html file
+      '/': {
+        target: 'http://localhost:5173/index.html', // Change this to match your index.html file location
+        changeOrigin: true,
+        rewrite: (path) => path === '/' ? '/index.html' : path,
+      },
+    },
+  },
 })
